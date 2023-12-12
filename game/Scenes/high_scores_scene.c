e6602b92a7f533f8d4bd6865d0d0e765082f4aa4
Jussi Enroos <jussi.enroos@gmail.com>
2023-12-12 18:55:41 +0200

    Add persistent high score storage by serialising to a file

--- game/Scenes/high_scores_scene.c
+++ game/Scenes/high_scores_scene.c
@@ -1,6 +1,7 @@
 #include "high_scores_scene.h"
 #include "game_data.h"
 #include "gecko_scene.h"
+#include "serialiser.h"
 
 #define MAX_NAME_LENGTH 5
 #define ENTRIES_COUNT 10
@@ -55,30 +56,33 @@ void high_scores_scene_show_scores(void *context)
             label_right->anchor = vec(0.f, 0.5f);
             label_right->position = vec(200.f, i * 18.f);
             label_right->draw_mode = drawmode_scale;
+            
+            const float squish_time = 0.19f;
+            const float flight_time = 0.4f;
 
             go_add_component(label_left, act_create(action_sequence_create(({
                 ArrayList *list = list_create();
                 list_add(list, action_delay_create(0.2f));
-                list_add(list, action_ease_in_create(action_move_to_create(vec(-8.f, i * 18.f), 0.5f)));
-                list_add(list, action_ease_out_create(action_scale_to_create(vec(0.5f, 2.f), 0.29f)));
-                list_add(list, action_ease_in_create(action_scale_to_create(vec(1.f, 1.f), 0.29f)));
+                list_add(list, action_ease_in_create(action_move_to_create(vec(-8.f, i * 18.f), flight_time)));
+                list_add(list, action_ease_out_create(action_scale_to_create(vec(0.5f, 2.f), squish_time)));
+                list_add(list, action_ease_in_create(action_scale_to_create(vec(1.f, 1.f), squish_time)));
                 list;
             }))));
             
             go_add_component(label_right, act_create(action_sequence_create(({
                 ArrayList *list = list_create();
                 list_add(list, action_delay_create(0.2f));
-                list_add(list, action_ease_in_create(action_move_to_create(vec(8.f, i * 18.f), 0.5f)));
-                list_add(list, action_ease_out_create(action_scale_to_create(vec(0.5f, 2.f), 0.29f)));
-                list_add(list, action_ease_in_create(action_scale_to_create(vec(1.f, 1.f), 0.29f)));
+                list_add(list, action_ease_in_create(action_move_to_create(vec(8.f, i * 18.f), flight_time)));
+                list_add(list, action_ease_out_create(action_scale_to_create(vec(0.5f, 2.f), squish_time)));
+                list_add(list, action_ease_in_create(action_scale_to_create(vec(1.f, 1.f), squish_time)));
                 list;
             }))));
         } else {
             label_left->anchor = vec(1.f, 0.5f);
-            label_left->position = vec(-10.f, i * 18.f);
+            label_left->position = vec(-8.f, i * 18.f);
             
             label_right->anchor = vec(0.f, 0.5f);
-            label_right->position = vec(10.f, i * 18.f);
+            label_right->position = vec(8.f, i * 18.f);
         }
     
         go_add_child(self->w_score_base, label_left);
@@ -99,9 +103,45 @@ void high_scores_scene_show_scores(void *context)
     }
 }
 
-void high_scores_scene_save_scores(HighScoresScene *self)
+void high_score_entry_serialise(Serialiser *ser, void *context)
 {
+    HighScoreEntry *entry = (HighScoreEntry *)context;
     
+    ser_write_str(ser, entry->name);
+    ser_write_int32(ser, entry->score);
+}
+
+void high_scores_scene_serialise_scores(Serialiser *serialiser, void *context)
+{
+    HighScoresScene *self = (HighScoresScene *)context;
+    
+    for (int i = 0; i < ENTRIES_COUNT; ++i) {
+        ser_write_obj_with_function(serialiser, &self->entries[i], &high_score_entry_serialise);
+    }
+}
+
+void *high_score_entry_deserialise(Deserialiser *deser)
+{
+    HighScoreEntry *entry = platform_calloc(1, sizeof(HighScoreEntry));
+    char *name = deser_read_str(deser);
+    strncpy(entry->name, name, MAX_NAME_LENGTH + 1);
+    entry->score = deser_read_int32(deser);
+    platform_free(name);
+    
+    return entry;
+}
+
+void *high_scores_scene_deserialise_scores(Deserialiser *deser)
+{
+    HighScoreEntry *entries = platform_calloc(ENTRIES_COUNT, sizeof(HighScoreEntry));
+    for (int i = 0; i < ENTRIES_COUNT; ++i) {
+        HighScoreEntry *entry = deser_read_obj_with_function(deser, &high_score_entry_deserialise);
+        strncpy(entries[i].name, entry->name, MAX_NAME_LENGTH + 1);
+        entries[i].score = entry->score;
+        platform_free(entry);
+    }
+    
+    return entries;
 }
 
 void high_scores_scene_check_for_new_high_score(HighScoresScene *self)
@@ -140,9 +180,9 @@ void high_scores_scene_check_for_new_high_score(HighScoresScene *self)
             go_add_component(label, act_create(action_sequence_create(({
                 ArrayList *list = list_create();
                 list_add(list, action_delay_create(0.5f));
-                list_add(list, action_ease_in_create(action_move_to_create(vec(SCREEN_WIDTH / 2.f, SCREEN_HEIGHT / 2.f + 10.f), 0.6f)));
-                list_add(list, action_ease_out_create(action_scale_to_create(vec(2.f, 0.5f), 0.29f)));
-                list_add(list, action_ease_in_create(action_scale_to_create(vec(1.f, 1.f), 0.29f)));
+                list_add(list, action_ease_in_create(action_move_to_create(vec(SCREEN_WIDTH / 2.f, SCREEN_HEIGHT / 2.f + 10.f), 0.4f)));
+                list_add(list, action_ease_out_create(action_scale_to_create(vec(2.f, 0.5f), 0.19f)));
+                list_add(list, action_ease_in_create(action_scale_to_create(vec(1.f, 1.f), 0.19f)));
                 list;
             }))));
 
@@ -156,7 +196,7 @@ void high_scores_scene_check_for_new_high_score(HighScoresScene *self)
         
         go_add_child(self, self->w_new_high_score_label);
         
-        high_scores_scene_save_scores(self);
+        serialise_to_file(HIGH_SCORE_FILE, self, &high_scores_scene_serialise_scores);
     }
         
     go_delayed_callback(self, &high_scores_scene_show_scores, 3.f);
@@ -171,12 +211,32 @@ void high_scores_scene_create_empty_high_scores(HighScoresScene *self)
     high_scores_scene_check_for_new_high_score(self);
 }
 
+void high_scores_scene_scores_deserialised(void *obj, void *context)
+{
+    HighScoresScene *self = (HighScoresScene *)context;
+    HighScoreEntry *entries = (HighScoreEntry *)obj;
+    
+    if (entries == NULL || sizeof(entries) == 0) {
+        high_scores_scene_create_empty_high_scores(self);
+    } else {
+        for (int i = 0; i < ENTRIES_COUNT; ++i) {
+            strncpy(self->entries[i].name, entries[i].name, MAX_NAME_LENGTH + 1);
+            self->entries[i].score = entries[i].score;
+        }
+        high_scores_scene_check_for_new_high_score(self);
+    }
+    
+    if (entries != NULL) {
+        platform_free(entries);
+    }
+}
+
 void high_scores_scene_scores_file_exists(const char *file_name, bool exists, void *context)
 {
     HighScoresScene *self = (HighScoresScene *)context;
     LOG("Score file %s exists: %s", file_name, exists ? "true" : "false");
     if (exists) {
-        // Load high scores
+        deserialise_file(HIGH_SCORE_FILE, &high_scores_scene_deserialise_scores, &high_scores_scene_scores_deserialised, self);
     } else {
         high_scores_scene_create_empty_high_scores(self);
     }
